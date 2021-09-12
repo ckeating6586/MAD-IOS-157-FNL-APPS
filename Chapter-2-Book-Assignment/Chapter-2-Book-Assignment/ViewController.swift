8bd8ce545648e8e15a1e93a96e3b7d25576d6aa9
Carmen Keating <by.carmen.keating@gmail.com>
2021-09-11 20:57:42 -0700

    My First App Ch2 Assignment

---
+++ Chapter-2-Book-Assignment/Chapter-2-Book-Assignment/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Book-Assignment-test
+//
+//  Created by Carmen on 9/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+/*---------------This is code to create UI objects from scratch------------------------*/
+        
+//        let label = UILabel()
+//        label.frame = CGRect(x: 125,y: 125, width: 200, height: 80)
+//        label.text = "This is a label"
+//        view.addSubview(label)
+//
+//        let button = UIButton()
+//        button.frame = CGRect(x: 125, y: 300, width: 80, height: 80)
+//        button.setTitle("Button", for: .normal)
+//        button.setTitleColor(UIColor.blue, for: .normal)
+//        view.addSubview(button)
+        
+        
+/*---------------This is code to create UI objects from UI kit------------------------*/
+    }
+
+}
+
