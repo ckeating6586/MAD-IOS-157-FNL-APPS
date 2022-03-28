b4b2d65b9173f180ecb227e970669dfc4f3f4039
Carmen Keating <by.carmen.keating@gmail.com>
2022-03-27 21:09:59 -0700

    Committing the grouped table view app

---
+++ GroupTableViewApp/GroupTableViewApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  GroupTableViewApp
+//
+//  Created by Carmen on 3/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var petTable: UITableView!
+    
+    let petArray = [["Mammal", "cat", "dog", "hamster", "gerbil", "rabbit"],["Bird", "parakeet", "parrot", "canary", "finch"], ["Fish", "Tropical Fish", "Goldfish", "sea horses"],["Reptile", "turtle", "Lizard", "Snake"]]
+    
+    let cellID = "cellID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        petTable.dataSource = self
+        petTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return petArray[section].count - 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        if (cell == nil) {
+            cell = UITableViewCell(
+                style: UITableViewCell.CellStyle.default, reuseIdentifier: cellID)
+        }
+        cell?.textLabel?.text = petArray[indexPath.section][indexPath.row + 1]
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedItem = petArray[indexPath.section][indexPath.row+1]
+        let alert = UIAlertController(title: "Your Choice", message: "\(selectedItem)", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: {action -> Void in})
+        alert.addAction(okAction)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return petArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section:Int) -> String? {
+        return petArray[section][0]
+    }
+
+
+}
+
